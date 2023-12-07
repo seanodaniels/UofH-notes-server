ceed7efec705f9d8761559279696e0e1026d33ca
Sean O'Daniels <sodaniels@gmail.com>
2023-12-07 09:47:26 -0500

    Prep for beginning of part 5.

--- controllers/users.js
+++ controllers/users.js
@@ -2,6 +2,15 @@ const bcrypt = require('bcrypt')
 const usersRouter = require('express').Router()
 const User = require('../models/user')
 
+usersRouter.get('/', async (request, response) => {
+  // Get only the users
+  // const users = await User.find({})
+
+  // Get the users and show their notes
+  const users = await User.find({}).populate('notes', { contents: 1, important: 1 })
+  response.json(users)
+})
+
 usersRouter.post('/', async (request, response) => {
   const { username, name, password } = request.body
 
